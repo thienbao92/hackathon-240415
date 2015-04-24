600ed919044e5b98a216533e323aeca541e0d116
thanhkhang2312 <thanhkhang2312@gmail.com>
2015-04-24 13:42:20 +0300

    commit index

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,10 +1,12 @@
 angular.module('myApp.controller',[])	
 
 .controller('StudentCtrl',function($scope,$http){
+	console.log("Hello");
 	 $http.get("/event").success(function(response){
-	 	//$scope.gets = response;
-	 	console.log(response);
+	 	$scope.gets = response;
+	 	console.log("response");
 	 })
 })
-
-
+.controller('StudentCtrl',function($scope,$http){
+	console.log("Hello");
+})

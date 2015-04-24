2dc3feb66abfe971e39bb26804ceaf05d9877eaf
thienbao92 <thienbao.vudao@yahoo.com>
2015-04-24 13:34:31 +0300

    commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
-var myApp = angular.module('myApp',['myApp.directive','myApp.service','myApp.controller'])
+var myApp = angular.module('myApp',['myApp.directive','myApp.service','myApp.controller']);
+
+
 
 
